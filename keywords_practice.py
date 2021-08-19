2b6ca67f4276be555b84cb91a9d06a069b6884ca
Yashnikam-10 <82996923+Yashnikam-10@users.noreply.github.com>
2021-08-19 13:00:20 +0530

    Add files via upload

---
+++ keywords_practice.py
@@ -0,0 +1,14 @@
+colors = ['red',
+          'blue',
+          'black']
+
+
+def sum():
+    """MULTILINE COMMENTS
+PYTHON PRACTICE"""
+    a = 10
+    print(a)
+
+
+sum()
+print(colors, sum.__doc__, sep='\n')
