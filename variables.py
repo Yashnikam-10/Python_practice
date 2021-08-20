be841550f9be3df7d38a3be9b4d07a7a9ac7b13d
Yashnikam-10 <82996923+Yashnikam-10@users.noreply.github.com>
2021-08-20 12:44:14 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,13 @@
+a = 100
+b = a
+print(id(a), a, id(b), b, sep='\n')
+b = 200
+a = 245
+print(id(a), a, id(b), b, type(a), sep='\n')
+
+
+name = "test-1"
+Name = "test-2"
+N_am_e = "test-3"
+
+print(name, Name, N_am_e)
